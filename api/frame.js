2cc884217da6c7b5c6ec188e62b8996c637a60ba
pedroosebastiao <101417565+pedroosebastiao@users.noreply.github.com>
2025-04-11 16:43:44 -0300

    Update frame.js

--- api/frame.js
+++ api/frame.js
@@ -4,7 +4,7 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
     res.status(200).json({
         frames: [
             {
-                image: "https://warpcast-unit-converter.vercel.app/cover.png", 
+                image: "https://play-lh.googleusercontent.com/ETByWAKTp9cQS-YA3z4dnGKRFs_Uwmvz2d8uhOzMDIeLAIfXtIvzqirh7OhLH8wpZJDN", 
                 buttons: [
                     { label: "Converter", action: "link", target: "https://warpcast-unit-converter.vercel.app" }
                 ]
